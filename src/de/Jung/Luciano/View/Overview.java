e53879bcb09859644b8682f3c84639098558c394
Luciano Jung <you@example.com>
2019-01-29 14:03:52 +0100

    remove old classes

--- src/de/Jung/Luciano/View/Overview.java
+++ src/de/Jung/Luciano/View/Overview.java
@@ -1,16 +1,12 @@
 package de.Jung.Luciano.View;
 
 import de.Jung.Luciano.Model.Model;
-import de.Jung.Luciano.WebsiteButton.WebsiteButton;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
